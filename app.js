b5c8fe46b13eb88784697900b210db49cbbf5801
hafsoo <hafsariazturk9@gmail.com>
2026-03-28 23:42:59 +0500

    limit exceed

--- app.js
+++ app.js
@@ -11,13 +11,10 @@ const cors = require("cors");
 const ErrorHandler = require("./middleware/error");
 const app = express();
 
-// Middleware
-//app.use("/uploads", express.static("uploads"));
-//app.use(express.json());
 app.use(cookieParser());
-//app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json({ limit: "20mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "20mb" }));
+
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // Enable CORS for frontend
 app.use(
