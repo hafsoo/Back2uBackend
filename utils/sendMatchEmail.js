2149ea5c121597f047f2025749399f7741ec6ea5
hafsoo <hafsariazturk9@gmail.com>
2026-04-10 18:49:09 +0500

    lost user email

--- utils/sendMatchEmail.js
+++ utils/sendMatchEmail.js
@@ -16,7 +16,7 @@ const sendMatchEmail = async ({
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
